46bc81814358b981f13ef6baafdac9f8f661e9a8
Matthew de Villiers <ST10048577@vgconnect.edu.za>
2022-11-05 16:57:07 +0200

    created RangedWeapons subclass and added code (unsure is it is all correct)

---
+++ GADE POE/RangedWeapon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_POE
+{
+    internal class RangedWeapon : Weapon
+    {
+        public enum Types
+        {
+            Rifle,
+            Longbow
+        }
+
+        public RangedWeapon(int XPosition, int YPosition, Types Weapons) : base(XPosition, YPosition)
+        {
+            if (Weapons == Types.Rifle)
+            {
+                WeaponDurability = 3;
+                WeaponRange = 3;
+                WeaponDamage = 5;
+                WeaponCost = 7;
+            }
+            if (Weapons == Types.Longbow)
+            {
+                WeaponDurability = 4;
+                WeaponRange = 2;
+                WeaponDamage = 4;
+                WeaponCost = 6;
+            }
+        }
+
+        public override string ToString()
+        {
+            return null;
+        }
+
+        public override int WeaponRange { get => base.WeaponRange; set => base.WeaponRange = 1; }
+    }
+}
